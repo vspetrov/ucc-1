b85738fd7b82568214594353f3e8d230d47bf0f1
Sergey Lebedev <sergeyle@nvidia.com>
2021-03-31 05:31:52 +0000

    UTIL: support for custom ops in mempool

--- src/utils/ucc_mpool.h
+++ src/utils/ucc_mpool.h
@@ -7,16 +7,39 @@
 #define UCC_MPOOL_H_
 
 #include "config.h"
+#include "ucc/api/ucc.h"
 #include <ucs/datastruct/mpool.h>
 #include "ucc_compiler_def.h"
 #include "ucc_spinlock.h"
 
-typedef struct ucc_mpool {
-    ucs_mpool_t pool;
-    ucc_thread_mode_t tm;
-    ucc_spinlock_t lock;
-} ucc_mpool_t;
-typedef ucs_mpool_ops_t ucc_mpool_ops_t;
+typedef struct ucc_mpool ucc_mpool_t;
+
+typedef struct ucc_mpool_ops {
+    ucc_status_t (*chunk_alloc)(ucc_mpool_t *mp, size_t *size_p, void **chunk_p);
+    void         (*chunk_release)(ucc_mpool_t *mp, void *chunk);
+    void         (*obj_init)(ucc_mpool_t *mp, void *obj, void *chunk);
+    void         (*obj_cleanup)(ucc_mpool_t *mp, void *obj);
+} ucc_mpool_ops_t;
+
+struct ucc_mpool {
+    ucs_mpool_t        super;
+    ucc_mpool_ops_t   *ucc_ops;
+    ucc_thread_mode_t  tm;
+    ucc_spinlock_t     lock;
+};
+
+ucc_status_t ucc_mpool_init(ucc_mpool_t *mp, size_t priv_size, size_t elem_size,
+                            size_t align_offset, size_t alignment,
+                            unsigned elems_per_chunk, unsigned max_elems,
+                            ucc_mpool_ops_t *ops, ucc_thread_mode_t tm,
+                            const char *name);
+
+void ucc_mpool_cleanup(ucc_mpool_t *mp, int leak_check);
+
+ucc_status_t ucc_mpool_hugetlb_malloc(ucc_mpool_t *mp, size_t *size_p,
+                                      void **chunk_p);
+
+void ucc_mpool_hugetlb_free(ucc_mpool_t *mp, void *chunk);
 
 static inline void* ucc_mpool_get(ucc_mpool_t *mp)
 {
@@ -24,7 +47,7 @@ static inline void* ucc_mpool_get(ucc_mpool_t *mp)
     if (UCC_THREAD_MULTIPLE == mp->tm) {
         ucc_spin_lock(&mp->lock);
     }
-    ret= ucs_mpool_get(&mp->pool);
+    ret= ucs_mpool_get(&mp->super);
     if (UCC_THREAD_MULTIPLE == mp->tm) {
         ucc_spin_unlock(&mp->lock);
     }
@@ -34,7 +57,7 @@ static inline void* ucc_mpool_get(ucc_mpool_t *mp)
 static inline void ucc_mpool_put(void *obj)
 {
     ucs_mpool_elem_t *elem = (ucs_mpool_elem_t*)obj - 1;
-    ucc_mpool_t *mp = ucc_container_of(elem->mpool, ucc_mpool_t, pool);
+    ucc_mpool_t *mp = ucc_container_of(elem->mpool, ucc_mpool_t, super);
     if (UCC_THREAD_MULTIPLE == mp->tm) {
         ucc_spin_lock(&mp->lock);
     }
@@ -42,41 +65,6 @@ static inline void ucc_mpool_put(void *obj)
     if (UCC_THREAD_MULTIPLE == mp->tm) {
         ucc_spin_unlock(&mp->lock);
     }
-}
-
-typedef void (*ucc_mpool_obj_init_fn_t)(ucc_mpool_t *mp, void *obj,
-                                        void *chunk);
-typedef void (*ucc_mpool_obj_cleanup_fn_t)(ucc_mpool_t *mp, void *obj);
-
-static inline ucc_status_t
-ucc_mpool_init(ucc_mpool_t *mp, size_t elem_size, size_t alignment,
-               unsigned elems_per_chunk, unsigned max_elems,
-               ucc_mpool_obj_init_fn_t    init_fn,
-               ucc_mpool_obj_cleanup_fn_t cleanup_fn,
-               ucc_thread_mode_t tm, const char *name)
-{
-    ucs_mpool_ops_t *ops = ucc_malloc(sizeof(*ops), "mpool_ops");
-    if (!ops) {
-        ucc_error("failed to allocate %zd bytes for mpool ops", sizeof(*ops));
-        return UCC_ERR_NO_MEMORY;
-    }
-
-    ops->chunk_alloc   = ucs_mpool_hugetlb_malloc;
-    ops->chunk_release = ucs_mpool_hugetlb_free;
-    ops->obj_init      = (void*)init_fn;
-    ops->obj_cleanup   = (void*)cleanup_fn;
-    ucc_spinlock_init(&mp->lock, 0);
-    mp->tm = tm;
-    return ucs_status_to_ucc_status(ucs_mpool_init(
-        &mp->pool, 0, elem_size, 0, alignment, elems_per_chunk, max_elems, ops, name));
-}
-
-static inline void ucc_mpool_cleanup(ucc_mpool_t *mp, int leak_check)
-{
-    ucs_mpool_ops_t *ops = mp->pool.data->ops;
-    ucs_mpool_cleanup(&mp->pool, leak_check);
-    ucc_spinlock_destroy(&mp->lock);
-    ucc_free(ops);
 }
 
 #endif
